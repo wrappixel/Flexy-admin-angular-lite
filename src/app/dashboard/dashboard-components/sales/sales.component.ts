68d890b0d2eaa3e7520c9a8d8e9e1949b140f8a8
Javiya Deep <patelaleis@gmail.com>
2022-03-17 12:30:13 +0530

    .

--- src/app/dashboard/dashboard-components/sales/sales.component.ts
+++ src/app/dashboard/dashboard-components/sales/sales.component.ts
@@ -1,5 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import * as apex from 'ng-apexcharts';
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexLegend,
+  ApexStroke,
+  ApexTooltip,
+  ApexAxisChartSeries,
+  ApexFill,
+  ApexXAxis,
+  ApexGrid
+} from 'ng-apexcharts';
+
+
+export interface activeusercardChartOptions {
+  series: ApexAxisChartSeries;
+  dataLabels: ApexDataLabels;
+  chart: ApexChart;
+  legend: ApexLegend;
+  xaxis: ApexXAxis;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+}
+
 
 @Component({
   selector: 'app-sales',
@@ -7,80 +34,66 @@ import * as apex from 'ng-apexcharts';
   styleUrls: ['./sales.component.scss'],
 
 })
-export class SalesComponent implements OnInit {
+export class SalesComponent {
 
-  series!: apex.ApexAxisChartSeries;
-  dataLabels!: apex.ApexDataLabels;
-  chart!: apex.ApexChart;
-  legend!: apex.ApexLegend;
-  xaxis!: apex.ApexXAxis;
-  grid!: apex.ApexGrid;
-  stroke!: apex.ApexStroke;
-  plotOptions!: apex.ApexPlotOptions;
-  fill!: apex.ApexFill;
-  tooltip!: apex.ApexTooltip;
+  @ViewChild("activeusercardchart") chart1: ChartComponent = Object.create(null);
+  public activeusercardChartOptions !: Partial<activeusercardChartOptions> | any;
 
-  ngOnInit(): void {
-    this.initializeChartOptions();
-  }
-
-  private initializeChartOptions(): void {
-
-    this.series = [
-      {
-        name: 'Ample Admin',
-        data: [355, 390, 300, 350, 390, 180, 355, 390, 300, 350, 390, 180],
-        color: "#fb9678",
+  constructor() {
+    // active users
+    this.activeusercardChartOptions = {
+      series: [
+        {
+          name: 'Ample Admin',
+          data: [355, 390, 300, 350, 390, 180, 355, 390, 300, 350, 390, 180],
+          color: "#fb9678",
+        },
+        {
+          name: 'Pixel Admin',
+          data: [280, 250, 325, 215, 250, 310, 280, 250, 325, 215, 250, 310],
+          color: "#03c9d7",
+        },
+      ],
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       },
-      {
-        name: 'Pixel Admin',
-        data: [280, 250, 325, 215, 250, 310, 280, 250, 325, 215, 250, 310],
-        color: "#03c9d7",
+      chart: {
+        toolbar: {
+          show: false,
+        },
+        type: 'bar',
+        height: 300,
+
       },
-    ];
-    
-    this.xaxis = {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    };
-    
-    this.chart = {
-      toolbar: {
+      legend: {
         show: false,
       },
-      type: 'bar',
-      height: 300,
-      
-    };
-    
-    
-    this.legend = {
-      show: false,
-    };
-    
-    this.tooltip = {
-      theme: "dark"
-    };
-    
-    this.grid = {
-      show: false,
-    };
 
-    this.dataLabels = {
-      enabled: false,
-    };
-
-    this.stroke = {
-      show: true,
-      width: 5,
-      colors: ['none']
-    };
-
-    this.plotOptions = {
-      bar: {
-        columnWidth: '45%',
-        borderRadius: 6,
+      tooltip: {
+        theme: "dark"
       },
-    };
+
+      grid: {
+        show: false,
+      },
+
+      dataLabels: {
+        enabled: false,
+      },
+
+      stroke: {
+        show: true,
+        width: 5,
+        colors: ['none']
+      },
+
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          borderRadius: 6,
+        },
+      },
+    }
   }
 
 

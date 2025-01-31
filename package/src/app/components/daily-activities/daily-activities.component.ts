0f65bfb1071b7ae080cbb8f2a63b050cf7fe9da2
rutvik <wrappiexlteam.2016@gmail.com>
2025-01-31 11:13:25 +0530

    ''

--- package/src/app/components/daily-activities/daily-activities.component.ts
+++ package/src/app/components/daily-activities/daily-activities.component.ts
@@ -1,52 +1,59 @@
 import { Component } from '@angular/core';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { MaterialModule } from '../../material.module';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { MaterialModule } from 'src/app/material.module';
 
-interface stats {
-    id: number;
-    time: string;
-    color: string;
-    title?: string;
-    subtext?: string;
-    link?: string;
+interface transactions {
+  id: number;
+  iconColor: string;
+  icon: string;
+  title: string;
+  subtitle: string;
+  amount: string;
+  status: string;
 }
 
 @Component({
     selector: 'app-daily-activities',
-    imports: [NgApexchartsModule, MaterialModule],
+    imports: [MaterialModule, TablerIconsModule],
     templateUrl: './daily-activities.component.html',
 })
 export class AppDailyActivitiesComponent {
-    stats: stats[] = [
+    transactions: transactions[] = [
         {
-            id: 1,
-            time: '09.30 am',
-            color: 'primary',
-            subtext: 'Payment received from John Doe of $385.90',
+          id: 1,
+          iconColor: 'secondary',
+          icon: 'currency-dollar',
+          title: 'PayPal Transfer',
+          subtitle: 'Money Added',
+          amount: '+$350',
+          status: 'success',
         },
         {
-            id: 2,
-            time: '10.30 am',
-            color: 'accent',
-            title: 'New sale recorded',
+          id: 2,
+          iconColor: 'success',
+          icon: 'shield',
+          title: 'Wallet',
+          subtitle: 'Bill Payment',
+          amount: '-$560',
+          status: 'error',
         },
         {
-            id: 3,
-            time: '12.30 pm',
-            color: 'success',
-            subtext: 'Payment was made of $64.95 to Michael',
+          id: 3,
+          iconColor: 'error',
+          icon: 'credit-card',
+          title: 'Credit Card',
+          subtitle: 'Money reversed',
+          amount: '+$350',
+          status: 'success',
         },
         {
-            id: 4,
-            time: '12.30 pm',
-            color: 'warning',
-            title: 'New sale recorded',
+          id: 4,
+          iconColor: 'warning',
+          icon: 'check',
+          title: 'Bank Transfer',
+          subtitle: 'Money Added',
+          amount: '+$350',
+          status: 'success',
         },
-        {
-            id: 5,
-            time: '12.30 pm',
-            color: 'error',
-            title: 'New arrival recorded',
-        },
-    ];
+      ];
 }
